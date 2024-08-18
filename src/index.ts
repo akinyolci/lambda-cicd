9b9f0df2ede22fb5c88a13ab0150cb74614d47d1
akinyolci <akinyolcu0@gmail.com>
2024-08-18 19:27:39 +0300

    updated index.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export const handler = async (event: any) => {
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: "Hello from Lambda by Jan!" }),
+      body: JSON.stringify({ message: "Hello from Lambda by Akin!" }),
     };
   };
